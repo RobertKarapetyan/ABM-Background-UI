0b9c09460e4d11fc4846cd0925d7201875b6f6ba
Robert Karapetyan <robertkarapetyan@artlover.com>
2018-05-06 18:04:01 -0700

    Add project files.

---
+++ Assets/Scripts/PatchController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatchController : MonoBehaviour
+{
+    public InitialUI InitialUI { get; set; }
+
+    private void Awake()
+    {
+        this.InitialUI = new InitialUI
+        {
+            Prefab = this.gameObject,
+            InitialPosition = new Vector2(-8.5f, 4.5f),
+            Matter = PrimitiveType.Cube, 
+            PatchCount = new int[] {10, 18}
+        };
+    }
+
+    void Start()
+    {
+        this.InitialUI.Generate();
+
+        //InitialUI.Patches[0].GetComponent<Renderer>().material.color = Color.blue;
+
+        //this.InitialUI.GetPatch(5, 2).GetComponent<Renderer>().material.color =
+        //    Color.blue;
+    }
+}
---
+++ Assets/Utilities/InitialUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using UnityEngine;
+
+public class InitialUI 
+{
+    public GameObject Prefab { get; set; }
+    public PrimitiveType Matter { get; set; }
+    public List<GameObject> Patches { get; set; }
+    public Vector2 InitialPosition { get; set; }
+    public int[] PatchCount { get; set; }
+
+    public InitialUI()
+    {
+        this.Patches = new List<GameObject>();
+    }
+
+    public void Generate()
+    {
+        for (int i = 0; i < PatchCount[0]; i++)
+        {
+            for (int j = 0; j < PatchCount[1]; j++)
+            {
+                GameObject c = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                c.transform.position = new Vector3(InitialPosition.x + (float)j,
+                    InitialPosition.y - (float)i, 0);
+                c.name = "Patch" + i.ToString() + "_" + j.ToString();
+                c.transform.parent = Prefab.transform;
+
+                Patches.Add(c);
+            }
+        }
+    }
+
+    public GameObject GetPatch(int x, int y)
+    {
+        return this.Patches[this.PatchCount[1] * y + x];
+    }
+}
+
